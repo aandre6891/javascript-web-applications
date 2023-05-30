9b8edaaa93951e9516e8e4a22e738133b6cf092a
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-05-30 16:49:47 +0100

    added #addParagraph

--- example-2/view.js
+++ example-2/view.js
@@ -1,8 +1,14 @@
 class View {
   constructor() {
-    this.mainContainerEl = document.querySelector('#main-container');
+    this.mainContainerEl = document.querySelector("#main-container");
 
     console.log(this.mainContainerEl);
+  }
+
+  addParagraph() {
+    const newParagraph = document.createElement("p");
+    newParagraph.innerText = "This paragraph has been dynamically added by JavaScript!";
+    this.mainContainerEl.append(newParagraph);
   }
 }
 
