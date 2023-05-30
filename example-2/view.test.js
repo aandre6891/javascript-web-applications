cad29dc8d5f63372795ffdb418f12990d5f2bf35
Andrea Ruggieri <ruggieri6891@gmail.com>
2023-05-30 17:24:28 +0100

    added clearParagraphs method and test

--- example-2/view.test.js
+++ example-2/view.test.js
@@ -6,11 +6,26 @@ const fs = require('fs');
 const View = require('./view');
 
 describe('Page view', () => {
-  it('displays 2 paragraphs', () => {
+  beforeEach(() => {
     document.body.innerHTML = fs.readFileSync('./index.html');
-
+  });
+  
+  it('displays 2 paragraphs', () => {
     const view = new View();
-
+    
     expect(document.querySelectorAll('p').length).toBe(2);
   });
+  
+  it('should add a new note and expect 3 notes', () => {
+    const view = new View();
+    view.addParagraph();
+    expect(document.querySelectorAll('p').length).toBe(3);
+  });
+  
+  it('it should clear all paragraphs and expect 0', () => {
+    const view = new View();
+    view.clearParagraphs();
+    expect(document.querySelectorAll('p').length).toBe(0);
+
+  })
 });
